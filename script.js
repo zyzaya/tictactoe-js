b4960b52b9882e587220bf2133354ef8c52b6fd5
Riley Sherlock <riley.sherlock@mts.net>
2022-03-02 13:57:06 -0600

    create cell and board objects

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const Player = (name, icon) => {
-  const getIcon = () => { return icon }
-  return {name, getIcon}
-}
+  const getIcon = () => { return icon };
+  return {name, getIcon};
+};
+
+const Cell = (div) => {
+  display = "";
+  let button = div.appendChild(document.createElement("button"));
+
+  const getDisplay = () => { return display };
+  const setDisplay = (text) => {
+    button.textContent = text;
+  };
+  return { getDisplay, setDisplay };
+};
+
+const Board = ((div) => {
+  let board = new Array(3).fill(new Array(3).fill(Cell(div)));
+  const onBoard = (col, row) => {
+    return col >= 0 && col <= 2 && row >= 0 && row <= 2;
+  };
+  const play = (col, row, player) => {
+    if (!onBoard(col, row)) {
+
+    } else if (board[col][row] != null) {
+
+    } else {
+      board[row][col].setDisplay(player.getIcon());
+      updateDisplay();
+      nextTurn();
+    }
+  };
+  return {
+    play
+  };
+})();
 
